7631e6925f9c46c2dc71ccde3aea264334519554
sano <taikisano1202@gmail.com>
2022-08-11 22:18:47 +0900

    Loop変数による一覧偶数行の色塗り

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -19,11 +19,19 @@
       <th>価格</th>
     </tr>
     @foreach ($books as $book)
-      <tr>
-        <td>{{$book->category->title}}</td>
-        <td>{{$book->title}}</td>
-        <td>{{$book->price}}</td>
-      </tr>
+      @if ($loop->even)
+        <tr style="background-color:#E0E0E0">
+          <td>{{$book->category->title}}</td>
+          <td>{{$book->title}}</td>
+          <td>{{$book->price}}</td>
+        </tr>
+      @else
+        <tr>
+          <td>{{$book->category->title}}</td>
+          <td>{{$book->title}}</td>
+          <td>{{$book->price}}</td>
+        </tr>
+      @endif
     @endforeach
   </table>
 </body>
